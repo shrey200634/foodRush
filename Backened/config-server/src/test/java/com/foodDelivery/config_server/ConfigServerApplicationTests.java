9bfe1aee8e70e0e5ff24f687ae36f5e56691f8c8
shrey200634 <dshrey60@gmail.com>
2026-03-26 01:46:57 +0530

    Initial setup: config-server, discovery-server, user-service

---
+++ Backened/config-server/src/test/java/com/foodDelivery/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodDelivery.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
